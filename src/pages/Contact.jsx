c5c4824847be95a91ac8f93f2602262a77fb3f57
Viren Gajjar <vgajjar1010@gmail.com>
2025-10-21 17:30:59 +0530

    feat: use render server instead of local host

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -32,7 +32,7 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
     try {
-      const response = await fetch('http://localhost:5000/contact', {
+      const response = await fetch('https://rudratrading-backend.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
